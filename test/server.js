ce42c625592bb9a4cd68261536a9e513c132c0bb
Mauro Leggieri <mxmauro@mauroleggieri.com>
2017-09-07 16:07:45 -0300

    Almost final code and working demo

--- test/server.js
+++ test/server.js
@@ -1,3 +1,9 @@
+/*!
+ * JsTemplate
+ *
+ * Copyright(c) 2017 Mauro H. Leggieri <mxmauro [at] mauroleggieri.com>
+ * MIT Licensed
+ */
 'use strict';
 
 var fs = require('fs');
@@ -11,58 +17,31 @@ var FileStore = require('session-file-store')(session);
 
 global.config = JSON.parse(fs.readFileSync(__dirname + path.sep + 'config.json', 'utf8'));
 
-/*
-global.async = require('async');
-global.entities = new (require('html-entities').AllHtmlEntities)();
-global.fs = require("fs");
-global.path = require('path');
-global.config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
-global.Web3 = require('./helpers/web3_connector');
-global.mysql = require('./helpers/mysql_connector');
-*/
 //------------------------------------------------------------------------------
 
 EnsureFolderExists(__dirname + path.sep + 'sessions');
-/*
-EnsureFolderExists(__dirname + path.sep + 'temp');
-*/
 
 var app = express();
-var jstemplate = require('../lib/jstemplate');
-
-app.engine('jst', jstemplate.__express);
-app.set('views', __dirname + path.sep + 'website');
-
+var jstemplate = require('../lib/jstemplate').createRenderer({
+	root: __dirname + path.sep + 'website',
+	showErrorDetails: true,
+	userData: {
+		randomInt: function (min, max) {
+			return Math.floor(Math.random() * (max - min) + min);
+		}
+	}
+});
 var staticFiles = express.static(__dirname + path.sep + 'website');
 
-/*
-// Router
-var router = express.Router();
-
-router.all('*', function (req, res, next) {
-    res.sendStatus(403);
-});
-
-// Middlewares
-/*
-app.use(multipart({
-    encoding: 'utf-8',
-    uploadDir: __dirname + path.sep + 'temp',
-    maxFieldsSize: 5 * 1024 * 1024
-}));
-*/
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: false, limit: '5mb' }));
-/*
-app.use(expressValidator({})); // this line must be immediately after any of the bodyParser middlewares!
-*/
 app.use(methodOverride());
 app.use(helmet());
 app.disable('x-powered-by');
 app.set('trust proxy', 1);
 app.use(session({
 	secret: 'jstemplate session magic',
-	name: 'jstsessionid',
+	name: 'jstsessionid',	
 	resave: false,
 	saveUninitialized: true,
 	cookie: {
@@ -76,8 +55,9 @@ app.use(session({
 }));
 
 app.get('*', function(req, res, next) {
-		jstemplate.render(req, res, next);
-	},function(req, res, next) {
+		jstemplate(req, res, next); //pass the request to 'next' if not a .jst file
+	},
+	function(req, res, next) {
 		staticFiles(req, res, next);
 	}
 );
@@ -91,6 +71,6 @@ app.listen(config.server.port, function() {
 
 function EnsureFolderExists(dir)
 {
-    if (!fs.existsSync(dir))
-        fs.mkdirSync(dir);
+	if (!fs.existsSync(dir))
+		fs.mkdirSync(dir);
 }
